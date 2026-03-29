5c49dfd6601acc59bc33ac88cdd206a42cfcafb9
bardstm32 <3448232613@qq.com>
2026-03-29 14:15:00 +0800

    删除了蜂鸣器的相关功能

--- Project/user/main.c
+++ Project/user/main.c
@@ -9,10 +9,10 @@ void main()
     PID_Init(&pos_pid,    2.8f,  0.15f, 0.6f,  POS_PID_MAX_OUT, POS_PID_MAX_I);
     PID_Init(&left_spid,  6.0f,  0.8f,  0.0f,  SPEED_PID_MAX_OUT, SPEED_PID_MAX_I);
     PID_Init(&right_spid, 6.0f,  0.8f,  0.0f,  SPEED_PID_MAX_OUT, SPEED_PID_MAX_I);
-    while(1){
+    while(1)
+    {
 		Menu_Display(ADC_value);
-		
-		    }
+	}
 }
 
 
